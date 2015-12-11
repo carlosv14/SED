c1c76aa555d1edae9ddfdf505ee87eb44dbad795
Luis Carlos Isaula <luis_isaula@hotmail.com>
2015-12-11 11:12:31 -0600

    Cambio de delete_button

--- SistemaEvaluador/Evaluador.Designer.cs
+++ SistemaEvaluador/Evaluador.Designer.cs
@@ -40,7 +40,6 @@
             this.Fecha = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.bVolver = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -151,7 +150,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(494, 24);
+            this.button1.Location = new System.Drawing.Point(494, 49);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 37);
@@ -163,7 +162,7 @@
             // bVolver
             // 
             this.bVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bVolver.Location = new System.Drawing.Point(494, 63);
+            this.bVolver.Location = new System.Drawing.Point(494, 89);
             this.bVolver.Margin = new System.Windows.Forms.Padding(2);
             this.bVolver.Name = "bVolver";
             this.bVolver.Size = new System.Drawing.Size(144, 28);
@@ -172,25 +171,12 @@
             this.bVolver.UseVisualStyleBackColor = true;
             this.bVolver.Click += new System.EventHandler(this.bVolver_Click);
             // 
-            // button2
-            // 
-            this.button2.Enabled = false;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(494, 96);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 30);
-            this.button2.TabIndex = 44;
-            this.button2.Text = "Borrar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Evaluador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(221)))), ((int)(((byte)(238)))));
             this.ClientSize = new System.Drawing.Size(758, 547);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.bVolver);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -232,6 +218,5 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ToolStripMenuItem formatoEvaluadorToolStripMenuItem;
         private System.Windows.Forms.Button bVolver;
-        private System.Windows.Forms.Button button2;
     }
 }
--- SistemaEvaluador/Evaluador.cs
+++ SistemaEvaluador/Evaluador.cs
@@ -432,11 +432,5 @@ namespace SistemaEvaluador
             if (this.shouldExit)
                 this.Close();
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //Boton borrar
-
-        }
     }
 }
